67ec92519296ac111aff850817f3e31ee2dc8a6a
Daniel1martinez2 <daniel.martinezvillegas2001@gmail.com>
2022-08-15 07:30:12 -0500

    init

---
+++ server.py
@@ -0,0 +1,28 @@
+import socket
+import time
+import pickle
+
+HEADER_SIZE = 10
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.bind(("192.168.0.15", 8080))
+s.listen(5)
+
+while True:
+  clientsocket, address = s.accept()
+  print(f"Connection from {address} has been established!")
+
+  msg = "Welcome to the server"
+  msg = f'{len(msg):< {HEADER_SIZE}}' +  msg
+
+  clientsocket.send(bytes(msg, "utf-8"))
+  full_msg = ''
+    
+  while True:
+    time.sleep(3)
+    msg = f"The time is: {time.time()}"
+    msg = f'{len(msg):< {HEADER_SIZE}}' +  msg
+    clientsocket.send(bytes(msg, "utf-8"))
+
+  # msg = s.recv(1024)
+  # print(msg.decode("utf-8"))
