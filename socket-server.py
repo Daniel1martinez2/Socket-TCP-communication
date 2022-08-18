8751a21bf152a92161f343664b066f8d09a6c34f
Daniel1martinez2 <daniel.martinezvillegas2001@gmail.com>
2022-08-18 09:52:13 -0500

    server and client but pro

--- socket-server.py
+++ socket-server.py
@@ -1,29 +1,66 @@
-# https://docs.python.org/3/library/socketserver.html#module-socketserver
 import socketserver
+from sqlite3 import connect
+import time
+import threading
+import socket
 
-class MyTCPHandler(socketserver.BaseRequestHandler):
-    """
-    The request handler class for our server.
+# class MyServer(socketserver.ThreadingMixIn, socketserver.TCPServer): pass
+# class MyTCPRequestHandler(socketserver.StreamRequestHandler):
+#     # handle() method will be called once per connection
+#     num = 0
+#     def handle(self):      
+#       addr = self.request.getpeername()
+#       print(f"{addr} connected")
+#       msg = self.rfile.readline().strip()
+#       print("Data Recieved from client is:".format(msg), msg)
+#       MyTCPRequestHandler.num += 1
+#       self.wfile.write(f"Hello Client {MyTCPRequestHandler.num}".encode())
+          
+# aServer = MyServer(("192.168.0.15", 8080), MyTCPRequestHandler)
+# print(f"Started: {aServer}")
+# # Listen for ever
+# aServer.serve_forever()
 
-    It is instantiated once per connection to the server, and must
-    override the handle() method to implement communication to the
-    client.
-    """
+import socket 
+import threading
 
-    def handle(self):
-      while True:
-        # self.request is the TCP socket connected to the client
-        self.data = self.request.recv(1024).strip()
-        print("{} wrote:".format(self.client_address[0]))
-        print(self.data)
-        # just send back the same data, but upper-cased
-        self.request.sendall(self.data.upper())
+HEADER = 64
+PORT = 8080
+SERVER = "172.30.78.75" 
+ADDR = (SERVER, PORT)
+FORMAT = 'utf-8'
+DISCONNECT_MESSAGE = "!DISCONNECT"
 
-if __name__ == "__main__":
-    HOST, PORT = "192.168.0.15", 8080
+server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+server.bind(ADDR)
 
-    # Create the server, binding to localhost on port 9999
-    with socketserver.TCPServer((HOST, PORT), MyTCPHandler) as server:
-        # Activate the server; this will keep running until you
-        # interrupt the program with Ctrl-C
-        server.serve_forever()
+def handle_client(conn, addr):
+    print(f"[NEW CONNECTION] {addr} connected.")
+
+    connected = True
+    while connected:
+        msg_length = conn.recv(HEADER).decode(FORMAT)
+        if msg_length:
+            msg_length = int(msg_length)
+            msg = conn.recv(msg_length).decode(FORMAT)
+            if msg == DISCONNECT_MESSAGE:
+                connected = False
+
+            print(f"[{addr}] {msg}")
+            conn.send("Msg received".encode(FORMAT))
+
+    conn.close()
+        
+
+def start():
+    server.listen()
+    print(f"[LISTENING] Server is listening on {SERVER}")
+    while True:
+        conn, addr = server.accept()
+        thread = threading.Thread(target=handle_client, args=(conn, addr))
+        thread.start()
+        print(f"[ACTIVE CONNECTIONS] {threading.activeCount() - 1}")
+
+
+print("[STARTING] server is starting...")
+start()
